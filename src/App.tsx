ff6c97159a73e2ea3a804241bc074c18875aa015
nagisettyramu007@gmail.com <nagisettyramu007@gmail.com>
2020-09-18 22:12:14 +0530

    initial commit for UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import './App.css';
+import Converter from "./component/currency-convertor/convertor";
+
+class App extends Component {
+  render()
+  {
+    return <Converter/>;
+  }
+}
+
+export default App;
